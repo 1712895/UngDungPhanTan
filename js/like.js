c8e05eddae01d6af1a7d534eae2875c21e6022a9
Pham Vu <1712923@student.hcmus.edu.vn>
2021-07-23 17:40:39 +0700

    update like

--- js/like.js
+++ js/like.js
@@ -1,50 +1,37 @@
 jQuery(function ($) {
-  $('.heart').click(function () {
-      $(this).toggleClass('red')
-  })
-})
-
-
-/*$(".heart").click(function () {
-    $(this).toggleClass("red");
-});*/
+  $(".heart").click(function () {
+      $(this).toggleClass("red");
+  });
+});
 
 /*
-// check for saved 'darkMode' in localStorage
-let like = localStorage.getItem('red'); 
-
-const likeToggle = $(this).querySelector('heart');
-
-const like = () => {
-  // 1. Add the class to the body
-  $(this).toggleClass("red");
-  // 2. Update darkMode in localStorage
-  localStorage.setItem('red', 'enabled');
-}
-
-const unlike = () => {
-  // 1. Remove the class from the body
-  $(this).toggleClass("heart");
-  // 2. Update darkMode in localStorage 
-  localStorage.setItem('red', null);
-}
- 
-// If the user already visited and enabled darkMode
-// start things off with it on
-if (like === 'enabled') {
-  like();
-}
-
-// When someone clicks the button
-likeToggle.addEventListener('click', () => {
-  // get their darkMode setting
-  like = localStorage.getItem('red'); 
-  
-  // if it not current enabled, enable it
-  if (like !== 'enabled') {
-    like();
-  // if it has been enabled, turn it off  
-  } else {  
-    unlike(); 
-  }
+$(".heart").click(function () {
+    $(this).toggleClass('red');
+    event.preventDefault();
 });*/
+
+jQuery(function ($) {
+$(".heart").click(function(){
+  var likeStorage = $(this).attr("id");
+  if($(this).hasClass("red")) {
+    localStorage.setItem(likeStorage, 'true');
+  } 
+  else {
+    localStorage.removeItem(likeStorage, 'true');
+  }
+});
+});
+
+jQuery(function ($) {
+$(".heart").each(function() {
+  var mainlocalStorage = $(this).attr("id");
+  if(localStorage.getItem(mainlocalStorage) == 'true') {
+    $(this).addClass("red");
+  }
+  else {
+    $(this).removeClass("red");
+  }
+});
+});
+                    
+                    
