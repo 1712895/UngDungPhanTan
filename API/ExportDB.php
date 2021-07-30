af29589b406107142be4326ecb13f73fc24890df
Tran Thi Diem Uyen <37771558+1712895@users.noreply.github.com>
2021-07-30 17:21:15 +0700

    update commit

--- API/ExportDB.php
+++ API/ExportDB.php
@@ -4,10 +4,19 @@ header("Content-Type: application/json");
 require_once("../config/config1.php");
 $db =connect();
 $records = $db->Post->find();
-echo json_encode(iterator_to_array(($records)));
+$array = json_decode($records, true);
+$items = $array->kitNew->items;
 
-
+foreach($items as $item){
+    echo "<tr>";
+    echo "<td>" . $item . "</td>";
+    echo "<td>" . $item->killCount . "</td>";
+    echo "<td>" . $item->killedByCount . "</td>";
+    echo "<td>" . $item->selected . "</td>";
+    echo "<td>" . $item->unlocked . "</td>";
+    echo "</tr>";
+}
 $fp = fopen('Post.json', 'w');
-fwrite($fp,json_decode(($records)));
+fwrite($fp,json_encode(iterator_to_array(($records))));
 fclose($fp);
 ?>
