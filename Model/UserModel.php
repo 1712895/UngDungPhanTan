5a1c0974387a21c11038506ef0613cb80271508f
Luong Tuong Vy <luongtuongvy1111999@gmail.com>
2021-07-30 14:27:49 +0700

    Update Profile
    Num of Posts
    Num of Answers
    Link Profile

--- Model/UserModel.php
+++ Model/UserModel.php
@@ -1,8 +1,11 @@
 <?php
+use MongoDB\Client;
 
+require('vendor/autoload.php');
 
 class UserModel
 {
+    public $_id;
     public $Address;
     public $Avatar;
     public $Birthday;
@@ -15,6 +18,7 @@ class UserModel
 
     function __construct()
     {
+        $this->_id = "";
         $this->Address = "";
         $this->Avatar = "";
         $this->Birthday = date_create('now')->format('Y-m-d H:i:s');
@@ -28,9 +32,32 @@ class UserModel
     public static function findbyID($_id) {
         $db = connect();
         return  $db->User->findOne(
-            ['_id' => $_id]
+            ['_id' => new MongoDB\BSON\ObjectId("$_id")]
             );
     }
+
+    public static function countNumofPost($_id) {
+        $db = connect();
+        //Query
+        $result = $db->Post->find(['id_User' => new MongoDB\BSON\ObjectId("$_id")]);
+        $count = 0;
+        foreach ($result as $row) {
+            $count += 1;
+        }
+        return $count;
+    }
+
+    public static function countNumofAns($_id) {
+        $db = connect();
+        //Query
+        $result = $db->Answer->find(['IDUser' => new MongoDB\BSON\ObjectId("$_id")]);
+        $count = 0;
+        foreach ($result as $row) {
+            $count += 1;
+        }
+        return $count;
+    }
+
     public static function FindUser($email,$password)
     {
         $db = connect();
@@ -39,13 +66,16 @@ class UserModel
               'Password' =>$password]
         );
     }
+
     public static function FindUserByEmail($email)
     {
         $db = connect();
         return $db->User->findOne(
             ['Email'=>$email]
         );
+
     }
+
     public static function updatePassword($email,$password)
     {
         $db = connect();
@@ -72,5 +102,7 @@ class UserModel
         ]);
         return $result;
     }
+
+    
 }
 ?>
